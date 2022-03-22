8327513627c0a25dc191adfbb5e6d8e09430b98e
Ike Grobben <62964735+ikegrobben@users.noreply.github.com>
2022-03-22 19:32:40 +0100

    creating scss id page

--- src/pages/ReportItem/ReportItem.js
+++ src/pages/ReportItem/ReportItem.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
+// Import components
+import Card from "../../components/Card/Card";
+import ContentHeader from "../../components/ContentHeader/ContentHeader";
+
+// import scss
+import "./ReportItem.scss";
+
 // Temp import for data
 import items from "../../services/report.json";
 
-function ReportItem() {
+function ReportItem({ logOut }) {
   const { id } = useParams();
   const fullReportItem = items.find((reportItem) => {
     return reportItem.id === id;
@@ -14,6 +21,28 @@ function ReportItem() {
 
   return (
     <div>
+      <ContentHeader
+        title={`Report Item - ${fullReportItem.date}<h2>${fullReportItem.status}</h2>`}
+        logOut={logOut}
+      />
+      <h2 className="sr-only">Statistics</h2>
+      <div className="cards report-item__cards">
+        <Card
+          boxSubject="Created by"
+          boxAmountNumber={fullReportItem.creator}
+          boxInfo=""
+        />
+        <Card
+          boxSubject="Location"
+          boxAmountNumber={fullReportItem.location}
+          boxInfo=""
+        />
+        <Card
+          boxSubject="Category"
+          boxAmountNumber={fullReportItem.category}
+          boxInfo=""
+        />
+      </div>
       <h1>{fullReportItem.creator}</h1>
       <p>{fullReportItem.content}</p>
       <button onClick={() => navigate(-1)}>go back</button>
